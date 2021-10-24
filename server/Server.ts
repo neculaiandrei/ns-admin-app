bc3e60750bbc031ab9da249931f607270490cac3
neculaiandrei <an_drey_curent@yahoo.com>
2021-10-24 19:39:27 +0300

    some deploy

--- server/Server.ts
+++ server/Server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { generateData } from './Data';
 import mysql from 'mysql';
+import path from 'path';
 const app = express();
 const port = 3001;
 
@@ -11,12 +12,19 @@ var connection = mysql.createConnection({
   database: 'ns_admin_app'
 })
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get('/api/data', (req, res) => {
   connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
     console.log('The solution is: ', rows[0].solution)
   });
   const data = generateData(100, 10);
   res.json(data);
+});
+
+app.get('*', (req, res) => {
+  var indexPath = path.resolve(__dirname, '../client/build', 'index.html');
+  res.sendFile(indexPath);
 });
 
 app.listen(port, () => {
