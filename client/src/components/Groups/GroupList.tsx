6b627f1539ab42e0f6e70a1df5da43a479c362dc
neculaiandrei <an_drey_curent@yahoo.com>
2021-10-25 18:13:21 +0300

    simplify groups page

--- client/src/components/Groups/GroupList.tsx
+++ client/src/components/Groups/GroupList.tsx
@@ -5,32 +5,18 @@ import { GroupRowItem } from "./GroupRowItem";
 import { PersonRowItem } from "./PersonRowItem";
 
 interface GroupListProps {
-  parentId: number | null;
+  groups: Group[],
+  persons: Person[]
   onSelect: (id: number) => void;
 }
 
 export const GroupList: React.FC<GroupListProps> = (props) => {
-  const { data } = useContext(StoreContext);
-  const items = useMemo(() => {
-    const result = {
-      groups: [] as Group[],
-      persons: [] as Person[]
-    };
-
-    result.groups = data.groups.filter(g => g.parentId === props.parentId);
-    
-    const l = data.links.filter(l => l.groupId === props.parentId).map(l => l.personId);
-    result.persons = data.persons.filter(p => l.indexOf(p.id) !== -1);
-
-    return result;
-  }, [props.parentId, data.groups, data.links, data.persons]);
-
   return (
     <ul className="list-group">
-      {items.groups.map(g => (
+      {props.groups.map(g => (
         <GroupRowItem group={g} onSelect={props.onSelect} key={g.id} />
       ))}
-       {items.persons.map(p => (
+       {props.persons.map(p => (
         <PersonRowItem person={p} key={p.id} /> 
       ))}
     </ul>
--- client/src/components/Groups/GroupsPage.tsx
+++ client/src/components/Groups/GroupsPage.tsx
@@ -2,8 +2,10 @@ import { useCallback, useContext, useMemo } from "react";
 import { useHistory } from "react-router";
 import { useGroupParentIdParam } from "../../hooks/useGroupParentIdParam";
 import { StoreContext } from "../App";
+import { Group, Person } from "../Models";
 import { GroupList } from "./GroupList";
 import { GroupsPageHeader } from "./GroupsPageHeader";
+
 
 export const GroupsPage = () => {
   let groupParentId = useGroupParentIdParam();
@@ -13,6 +15,20 @@ export const GroupsPage = () => {
   const group = useMemo(() => {
     return data.groups.find(g => g.id === groupParentId)
   }, [groupParentId, data.groups]);
+
+  const { groups, persons } = useMemo(() => {
+    const result = {
+      groups: [] as Group[],
+      persons: [] as Person[]
+    };
+
+    result.groups = data.groups.filter(g => g.parentId === groupParentId);
+    
+    const l = data.links.filter(l => l.groupId === groupParentId).map(l => l.personId);
+    result.persons = data.persons.filter(p => l.indexOf(p.id) !== -1);
+
+    return result;
+  }, [groupParentId, data]);
 
   const goBack = useCallback(() => {
     const g = data.groups.find(g => g.id === groupParentId);
@@ -31,7 +47,7 @@ export const GroupsPage = () => {
   return (
     <div className="ns-groups-page">
       <GroupsPageHeader group={group} goBack={goBack} />
-      <GroupList parentId={groupParentId} onSelect={goForward}/>
+      <GroupList groups={groups} persons={persons} onSelect={goForward}/>
     </div>
   );
 };
